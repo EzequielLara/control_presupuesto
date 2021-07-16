b166a90be22c68d32230dcfcd7d37fa38c7cbf6f
Ezequiel <ezekdaw@gmail.com>
2021-07-16 15:51:25 +0200

    primer commit de inicio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Pregunta from "./components/Pregunta";
+import './index.css'
+
+function App() {
+  return (
+    <div className = 'header'>
+      <header>
+        <h1>Gasto Semanal</h1>
+        <Pregunta/>
+      </header>
+   </div>
+  );
+}
+
+export default App;
